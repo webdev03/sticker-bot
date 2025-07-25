635863ee1bc8f68d3b3faa90616d359cfe98e8bb
Devarsh <75148774+webdev03@users.noreply.github.com>
2025-07-25 19:17:06 +1200

    feat(web): move sticker card displaying to component

---
+++ apps/web/src/lib/components/StickerCard.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { ClipboardCopy } from "@lucide/svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { toast } from "svelte-sonner";
+
+  import type { Sticker } from "../../routes/(authed)/app/api/stickers/+server";
+
+  function copy(text: string) {
+    toast.promise(navigator.clipboard.writeText(text), {
+      loading: "Copying...", // this shouldn't ever show up
+      success: "Copied to clipboard!",
+      error: "Failed to copy to clipboard!",
+    });
+  }
+
+  let { sticker }: { sticker: Sticker } = $props();
+</script>
+
+<div class="break-inside-avoid rounded-xl bg-white p-4 shadow">
+  <div class="flex">
+    <div class="w-full">
+      <a href={sticker.slackPermalink} target="_blank" class="font-semibold"
+        >{sticker.title}</a
+      >
+      <!-- it's passed as a string -->
+      <p class="text-sm text-gray-500">
+        {new Date(sticker.createdAt).toLocaleString()}
+      </p>
+      <p class="text-xs text-gray-400">{sticker.width}x{sticker.height}</p>
+    </div>
+    <div>
+      <Button
+        variant="ghost"
+        onclick={() =>
+          copy(
+            sticker.emojis
+              .map((x) => `:${x}:`)
+              .map((x, i) => {
+                if ((i + 1) % sticker.width === 0) return x + "\n";
+                return x;
+              })
+              .join(""),
+          )}><ClipboardCopy /></Button
+      >
+    </div>
+  </div>
+
+  <div
+    class="mt-3 grid w-max max-w-full"
+    style={`grid-template-columns: repeat(${sticker.width}, minmax(0, 1fr));`}
+  >
+    {#each sticker.emojis as emoji, i}
+      <img
+        class="emoji-sync inline p-0 m-0 h-8 w-8"
+        src={`https://cachet.dunkirk.sh/emojis/${emoji}/r`}
+        alt={`Emoji ${i + 1}`}
+      />
+    {/each}
+  </div>
+</div>
--- apps/web/src/routes/(authed)/app/stickers/+page.svelte
+++ apps/web/src/routes/(authed)/app/stickers/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
   import { onMount, tick } from "svelte";
-  import { ClipboardCopy, Loader } from "@lucide/svelte";
-  import { Button } from "$lib/components/ui/button";
-  import { toast } from "svelte-sonner";
+  import { Loader } from "@lucide/svelte";
+  import StickerCard from "$lib/components/StickerCard.svelte";
 
   import type { Sticker } from "../api/stickers/+server";
 
@@ -10,14 +9,6 @@
   let loading = $state(true);
 
   let intersectionCanary: HTMLDivElement;
-
-  function copy(text: string) {
-    toast.promise(navigator.clipboard.writeText(text), {
-      loading: "Copying...", // this shouldn't ever show up
-      success: "Copied to clipboard!",
-      error: "Failed to copy to clipboard!",
-    });
-  }
 
   onMount(async () => {
     stickers = await (await fetch("/app/api/stickers")).json();
@@ -74,48 +65,7 @@
 
 <div class="columns-1 md:columns-2 xl:columns-3 gap-3 p-4 py-6">
   {#each stickers as sticker}
-    <div class="mb-4 break-inside-avoid rounded-xl bg-white p-4 shadow">
-      <div class="flex">
-        <div class="w-full">
-          <a href={sticker.slackPermalink} target="_blank" class="font-semibold"
-            >{sticker.title}</a
-          >
-          <!-- it's passed as a string -->
-          <p class="text-sm text-gray-500">
-            {new Date(sticker.createdAt).toLocaleString()}
-          </p>
-          <p class="text-xs text-gray-400">{sticker.width}x{sticker.height}</p>
-        </div>
-        <div>
-          <Button
-            variant="ghost"
-            onclick={() =>
-              copy(
-                sticker.emojis
-                  .map((x) => `:${x}:`)
-                  .map((x, i) => {
-                    if ((i + 1) % sticker.width === 0) return x + "\n";
-                    return x;
-                  })
-                  .join(""),
-              )}><ClipboardCopy /></Button
-          >
-        </div>
-      </div>
-
-      <div
-        class="mt-3 grid w-max max-w-full"
-        style={`grid-template-columns: repeat(${sticker.width}, minmax(0, 1fr));`}
-      >
-        {#each sticker.emojis as emoji, i}
-          <img
-            class="emoji-sync inline p-0 m-0 h-8 w-8"
-            src={`https://cachet.dunkirk.sh/emojis/${emoji}/r`}
-            alt={`Emoji ${i + 1}`}
-          />
-        {/each}
-      </div>
-    </div>
+    <StickerCard {sticker} />
   {:else}
     {#if !loading}
       <p>No stickers yet! You can be the first!</p>
